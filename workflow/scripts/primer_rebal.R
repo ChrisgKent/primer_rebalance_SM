5bddb308e0f13587c38e7c2005a2c2d57b77496a
ChrisgKent <chrisgkent@gmail.com>
2022-03-12 17:08:05 +0000

    Added the ability to set a pool volume, and also saved the median coverage data

--- workflow/scripts/primer_rebal.R
+++ workflow/scripts/primer_rebal.R
@@ -5,12 +5,28 @@ bed_dir = snakemake@input[["bed_dirs"]]
 amplicon_dir = snakemake@input[["amp_dir"]]
 n_sm = snakemake@params[["n"]] %>% 
   as.numeric()
-ul_of_each_primer = snakemake@params[["ul_of_each_primer"]] %>% 
+total_ul_of100mM = snakemake@params[["total_pool_ul"]] %>% 
   as.numeric()
 normalised_n_cap = snakemake@params[["normalised_n_cap"]] %>%
   as.numeric()
 ul_primer_10uM_used_in25ul_pcr = snakemake@params[["ul_of_10uM_primer_stock_in_25ul"]] %>% 
   as.numeric()
+
+# Functions
+pool_seperator = function(data, num){
+  filter(data, amplicon %%2 == num) %>% 
+    select(amplicon,median_coverage_prop)
+}
+
+primer_norm = function(pool,n=3,limit=10){
+  # n=3 for ARTIC style primers
+  # limit of normalaised_n is deemed to be 10.
+  data = pool %>% mutate(n = median_coverage_prop**(-1/n_sm),
+                         normalised_n = n / min(n))
+  # normalised_n is limited to 10
+  data$normalised_n[data$normalised_n > normalised_n_cap] = normalised_n_cap
+  data
+}
 
 # Readings in the amplicon data
 amplicon = read_delim(amplicon_dir, col_names = FALSE,show_col_types = FALSE)
@@ -60,43 +76,34 @@ plot = median_prop %>%
   labs(x = "Median(proportion amplicon count) / expected proportion")
 
 
-pool_seperator = function(data, num){
-  filter(data, amplicon %%2 == num) %>% 
-    select(amplicon,median_coverage_prop)
-}
 
+# Sep the primers into there pools 
 pool1 = pool_seperator(median_prop, 1)
 pool2 = pool_seperator(median_prop, 0)
 
-primer_norm = function(pool,n=3,limit=10){
-  # n=3 for ARTIC style primers
-  # limit of normalaised_n is deemed to be 10.
-  
-  data = pool %>% mutate(n = median_coverage_prop**(-1/n_sm),
-                         normalised_n = n / min(n))
-  # normalised_n is limited to 10
-  data$normalised_n[data$normalised_n > normalised_n_cap] = normalised_n_cap
-  data
-}
-
+# Normalise the Primers
 n_pool1 = primer_norm(pool1)
 n_pool2 = primer_norm(pool2)
 
-vol_cal = function(x){mutate(x,ul_of_each_primer,
-                             amplicon_added = normalised_n*ul_of_each_primer*2,
-                             each_primer_ul = amplicon_added/2,
-                             prop = amplicon_added/sum(amplicon_added),
-                             primer_conc_uM = each_primer_ul*100/sum(amplicon_added),
-                             pcr_conc_nM = ul_primer_10uM_used_in25ul_pcr/10*primer_conc_uM/25 * 1000) %>%
-    select(amplicon,median_coverage_prop,normalised_n,amplicon_added,each_primer_ul,pcr_conc_nM)}
+vol_cal = function(x){mutate(x,
+                             rough_amplicon_added = normalised_n*10*2) %>% 
+    mutate(amplicon_added = rough_amplicon_added*total_ul_of100mM/sum(rough_amplicon_added),
+           each_primer_ul = amplicon_added/2,
+           prop = amplicon_added/sum(amplicon_added),
+           primer_conc_uM = each_primer_ul*100/sum(amplicon_added),
+           pcr_conc_nM = ul_primer_10uM_used_in25ul_pcr/10*primer_conc_uM/25 * 1000) %>%
+    select(amplicon,median_coverage_prop,normalised_n,amplicon_added,each_primer_ul,pcr_conc_nM)
+  }
 
 vol_added_1 = vol_cal(n_pool1)
 vol_added_2 = vol_cal(n_pool2)
 
+
 output = cbind(vol_added_1,vol_added_2) %>%
-  .[,c(1,2,5,6,7,8,10,11)] 
-names(output) = c("amplicon_1","median_coverage_prop", "ul_of_each_primer_1","pcr_conc_nM_1", "amplicon_2","median_coverage_prop_2", "ul_of_each_primer_2","pcr_conc_nM_2")
+  .[,c(1,5,6,7,11,12)] 
+names(output) = c("amplicon_1", "ul_of_each_primer_1","pcr_conc_nM_1", "amplicon_2", "ul_of_each_primer_2","pcr_conc_nM_2")
 output = output %>% mutate(across(.cols = everything(), signif, 3))
+
 write_tsv(output, snakemake@output[["tsv"]])
 
 # Save plot 
@@ -107,5 +114,7 @@ ggsave(plot = plot,
        units = "in",
        device = "png")
 
+# Output median Coverage data
+write_tsv(median_prop, snakemake@output[["coverage"]])
   
   
