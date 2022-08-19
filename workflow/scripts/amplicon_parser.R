640b1e65cea9aefe83dbcd0b9bf36ad5a4f40e0e
ChrisgKent <chrisgkent@gmail.com>
2022-08-19 12:58:50 +0100

    Added a regex match for the primer name to get the primer ID. This is a more general case that is required for current work

--- workflow/scripts/amplicon_parser.R
+++ workflow/scripts/amplicon_parser.R
@@ -6,8 +6,14 @@ primer_scheme = read_delim(primer_scheme_dir,
                            col_names = FALSE,
                            show_col_types = FALSE)
 
+# This parses the amplicon name into the amplicon number and the direction
+primer_scheme = primer_scheme %>% 
+  mutate(amplicon_name = str_extract(X4, "[0-9]*_[LEFTRIGHT]*$")) %>%
+  separate(amplicon_name, c('number', 'Direction')) %>%
+  mutate(number = as.numeric(number))
+
 amplicons = primer_scheme %>% 
-  mutate(amplicon = rep(1:(nrow(primer_scheme)/2),2) %>% sort) %>%
+  mutate(amplicon = number) %>%
   group_by(amplicon) %>%
   summarise(start = min(X2),
             end = max(X3)) %>%
